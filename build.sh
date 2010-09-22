76b5693088891cbdea285e4a49e61ef453a5c432
aplsek <aplsek@gmail.com>
2010-09-22 15:42:01 -0400

    -n SCJChecker - compilation build script updated
    - type ./build.sh to compile the script and the testsuite.

--- build.sh
+++ build.sh
@@ -8,5 +8,17 @@ BUILD=./build
 rm -rf $BUILD
 mkdir $BUILD
 
-find ./src -name "*.java" | xargs javac -cp lib/my-checkers.jar:lib/langtools.jar:lib/scjChecker.jar:lib/build/:../../../../lib/scj.jar:lib/jsr308-all.jar:lib/checkers.jar: -d $BUILD
+echo "Compiling SCJChercker"
+find ./src -name "*.java" | xargs ./localbin/checkers/binary/javac -cp lib/my-checkers.jar:lib/langtools.jar:lib/scjChecker.jar:lib/build/:../../../../lib/scj.jar:lib/jsr308-all.jar:lib/checkers.jar: -d $BUILD
 
+
+cd build/ && find . -name "*.class" | xargs jar cf ../SCJChecker.jar && cd ..
+
+mv SCJChecker.jar lib/
+
+echo "SCJ-Checker installation completed."
+
+#test-suite
+echo "SCJChecker Test Suite compilation..."
+
+cd testsuite && ./build.sh
