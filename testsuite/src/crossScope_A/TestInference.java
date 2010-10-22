b3fb4c529912d3d52c1c8aacfaa19d23387d2098
aplsek <aplsek@gmail.com>
2010-10-22 14:51:28 -0400

    -n Checker - updating my file (test)

--- testsuite/src/crossScope_A/TestInference.java
+++ testsuite/src/crossScope_A/TestInference.java
@@ -1,26 +1,91 @@
+//crossScope_A/TestInference.java:64: error message.
+//        foo.methodErr(bar); 
+//                      ^
+//1 error
+
 package crossScope_A;
 
+import javax.realtime.PeriodicParameters;
+import javax.realtime.PriorityParameters;
+import javax.safetycritical.ManagedMemory;
+import javax.safetycritical.Mission;
+import javax.safetycritical.PeriodicEventHandler;
+import javax.safetycritical.StorageParameters;
 import javax.safetycritical.annotate.Allocate;
 import javax.safetycritical.annotate.CrossScope;
+import javax.safetycritical.annotate.DefineScope;
+import javax.safetycritical.annotate.RunsIn;
+import javax.safetycritical.annotate.SCJRestricted;
+import javax.safetycritical.annotate.Scope;
 
 
 
-public class TestInference {
+@Scope("crossScope_A.TestInference") 
+public class TestInference extends Mission {
 
+	public Foo foo;
+	
+    protected
+    void initialize() { 
+        new MyHandler(null, null, null, 0, this);
+    }
+
+    @Override
+    public long missionMemorySize() {
+        return 0;
+    }
+
+    @Allocate({THIS})
+    public Foo getFoo() {
+    	return this.foo;
+    }
+    
+}
+
+
+@Scope("crossScope_A.MyMission")  
+@RunsIn("crossScope_A.MyHandler") 
+class MyHandler extends PeriodicEventHandler {
+
+	private TestInference mission;
+	
+    public MyHandler(PriorityParameters priority,
+            PeriodicParameters parameters, StorageParameters scp, long memSize, TestInference mission) {
+        super(priority, parameters, scp, memSize);
+        
+        this.mission = mission;
+    }
+
+    public
+    void handleEvent() {
+        Foo foo = mission.getFoo();
+        Bar bar = new Bar();
+        
+        foo.field = bar;
+    }
+
+
+    @Override
+    public StorageParameters getThreadConfigurationParameters() {
+        return null;
+    }
 }
 
 
 
 class Foo {
 
-	Bar x;
+	public Bar field;
 
+	@Allocate({CURRENT})
     @CrossScope
 	public Bar method(Bar bar) {
-		
 		return bar;
 	}
 	
+	public Bar methodErr(Bar bar) {
+		return null;
+	}
 	
 	
 	@Allocate({THIS})
@@ -30,6 +95,7 @@ class Foo {
 }
 
 
-class Bar {
+
+class Bar_TestInference {
 	
 }
