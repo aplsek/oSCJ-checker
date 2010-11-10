b1ca6aa3ef084f7b6458a7982b2d4463012e71f8
aplsek <aplsek@gmail.com>
2010-11-10 15:12:16 -0500

    -n checker  test cases - TestGuard test case

---
+++ testsuite/src/copyInOut/TestGuard.java
@@ -0,0 +1,32 @@
+package copyInOut;
+
+import javax.realtime.MemoryArea;
+import javax.realtime.ScopedMemory;
+import javax.safetycritical.annotate.CrossScope;
+
+public class TestGuard {
+	
+	Foo foo;
+	
+	@CrossScope
+	public Foo method(Foo h) {
+		Foo c = new Foo();
+		Foo c2 = new Foo();
+		
+		final MemoryArea memC = ScopedMemory.getMemoryArea(c);
+		final MemoryArea memC2 = ScopedMemory.getMemoryArea(c2);
+		final MemoryArea memFoo = ScopedMemory.getMemoryArea(foo);
+		if (memC == memFoo) { 										// ---> GUARD
+			c = foo;												// OK
+		}
+		if (memC == memFoo) { 										// ---> GUARD
+			c2 = foo;												// ERROR
+		}
+		if (memC2 == memFoo) { 										// ---> GUARD
+			c = foo;												// OK
+		}
+		return c;
+	}
+	
+	class Foo {}
+}
