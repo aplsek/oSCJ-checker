c1e2c2ccc224f7f2608e25005b506351a289b27f
aplsek <aplsek@gmail.com>
2010-11-29 18:55:09 -0500

    -n Checker - ManagedThread Example, initial commit

---
+++ testsuite/src/copyInOut/ManagedThreadExample.java
@@ -0,0 +1,95 @@
+package copyInOut;
+
+import static javax.safetycritical.annotate.Level.LEVEL_2;
+import static javax.safetycritical.annotate.Phase.INITIALIZATION;
+import static javax.safetycritical.annotate.Scope.UNKNOWN;
+
+import javax.realtime.MemoryArea;
+import javax.realtime.PeriodicParameters;
+import javax.realtime.PriorityParameters;
+import javax.safetycritical.Mission;
+import javax.safetycritical.PeriodicEventHandler;
+import javax.safetycritical.StorageParameters;
+import javax.safetycritical.annotate.RunsIn;
+import javax.safetycritical.annotate.SCJAllowed;
+import javax.safetycritical.annotate.SCJRestricted;
+import javax.safetycritical.annotate.Scope;
+
+public class ManagedThreadExample {
+
+}
+
+class ManagedThread {
+
+	//@MemoryAreaEncloses(inner = { "this", "this", "this" }, outer = {
+	//		"schedule", "mem_info", "logic" })
+	@SCJAllowed(LEVEL_2)
+	@SCJRestricted(INITIALIZATION)
+	public ManagedThread(PriorityParameters priority,
+			StorageParameters mem_info, Runnable logic) {
+		// ....
+	}
+
+}
+
+
+@Scope("Immortal")
+class ImmortalEntry {
+	
+	MyManagedRunnable logic;
+	PriorityParameters pp;
+	StorageParameters mem;
+	
+}
+
+
+@Scope("MyOtherMission")
+class MyOtherMission extends Mission {
+
+	MyManagedRunnable logic;
+	PriorityParameters pp;
+	StorageParameters mem;
+	
+	public void initialize() {
+		MyHandler handler = new MyHandler();
+		
+		ManagedThread mThread = new ManagedThread(pp, mem, logic);
+	}
+
+	@Override
+	public long missionMemorySize() {
+		return 0;
+	}
+}
+
+@Scope("MyOtherMission")
+@RunsIn("MyHandler")
+class MyHandler extends PeriodicEventHandler {
+	public MyHandler() {
+		super(null, null, null, 0);
+	}
+
+	public MyHandler(PriorityParameters priority, PeriodicParameters period,
+			StorageParameters storage, long size) {
+		super(priority, period, storage, size);
+	}
+
+	@Scope(UNKNOWN)
+	public LinkedList list;
+
+	public void handleEvent() {
+		// ....
+	}
+
+	@Override
+	public StorageParameters getThreadConfigurationParameters() {
+		return null;
+	}
+}
+
+
+class MyManagedRunnable implements Runnable {
+	public void run() {
+		//....
+	}
+}
