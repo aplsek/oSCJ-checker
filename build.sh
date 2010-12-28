0a6d72e926a43457248a0503d7fdc818e1cffee5
aplsek <aplsek@gmail.com>
2010-12-28 15:39:26 -0500

    -n Examples - minor changes
    testsuite - minor changes
    
    Checker
    - readme file added
    - build file minor changes

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ echo "Compiling SCJChercker"
 find ./src -name "*.java" | xargs ./localbin/checkers/binary/javac -cp lib/my-checkers.jar:lib/langtools.jar:lib/scjChecker.jar:lib/build/:../../../../lib/scj.jar:lib/jsr308-all.jar:lib/checkers.jar: -d $BUILD
 
 
-cd build/ && find . -name "*.class" | xargs jar cf ../SCJChecker.jar && cd ..
+cd $BUILD && find . -name "*.class" | xargs jar cf ../SCJChecker.jar && cd ..
 
 mv SCJChecker.jar lib/
 
@@ -21,4 +21,4 @@ echo "SCJ-Checker installation completed."
 #test-suite
 echo "SCJChecker Test Suite compilation..."
 
-cd testsuite && ./build.sh
+#cd testsuite && ./build.sh
---
+++ checkHello.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+set -x
+
+FILES=`find ../../examples/helloworld -name "*.java"` 
+
+echo $FILES
+
+./localbin/checkers/binary/javac -proc:only -Awarns -cp ../../../../lib/scj.jar:lib/SCJChecker.jar  -processor checkers.SCJChecker  $FILES
+
+
