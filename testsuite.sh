f2fa40cbafa9a63ad64810f60a9c84114cd38a9f
aplsek <aplsek@gmail.com>
2011-01-26 14:22:18 -0500

    -n Checker - testsuite run script updated, readme updated

--- testsuite.sh
+++ testsuite.sh
@@ -9,5 +9,5 @@ TESTSUITE=./testsuite
 cd $TESTSUITE && ./clean.sh && ./build.sh && cd ..
 
 
-python localbin/runtests.py  --scj-path ../../../../lib/scj.jar --directory testsuite
+python localbin/runtests.py  --scj-path ./lib/scj.jar --directory testsuite
 
