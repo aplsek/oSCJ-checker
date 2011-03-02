093d4e0bf77a8ef97332352445c62f61219b18dc
Daniel Tang <dytang@cs.purdue.edu>
2011-03-02 11:04:18 -0500

    -n fix test case warning

--- testsuite/src/scope/defineScope/simple/TestReservedScopeNameParentImmortal.java
+++ testsuite/src/scope/defineScope/simple/TestReservedScopeNameParentImmortal.java
@@ -1,9 +1,10 @@
 package scope.defineScope.simple;
 
+import javax.safetycritical.Mission;
 import javax.safetycritical.annotate.DefineScope;
 import javax.safetycritical.annotate.Scope;
 
 public class TestReservedScopeNameParentImmortal {
     @DefineScope(name="a", parent=Scope.IMMORTAL)
-    static class X { }
+    static abstract class X extends Mission { }
 }
