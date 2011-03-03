81087e87644bae219c21d1631218db9d9c8ac6ed
Daniel Tang <dytang@cs.purdue.edu>
2011-03-03 11:15:54 -0500

    -n removing old-style tests that came back with the merge

--- testsuite/src/scope/scope/simple/TestMemoryAreaDefineScopeNotConsistent.java
+++ testsuite/src/scope/scope/simple/TestMemoryAreaDefineScopeNotConsistent.java
@@ -1,19 +1,17 @@
 package scope.scope.simple;
 
-import static javax.safetycritical.annotate.Scope.IMMORTAL;
-
 import javax.safetycritical.ManagedMemory;
 import javax.safetycritical.Mission;
 import javax.safetycritical.annotate.DefineScope;
 import javax.safetycritical.annotate.Scope;
 
 public abstract class TestMemoryAreaDefineScopeNotConsistent {
-    @DefineScope(name="a", parent=IMMORTAL)
+    @DefineScope(name="a", parent=Scope.IMMORTAL)
     static abstract class X extends Mission { }
 
     @Scope("a")
     @DefineScope(name="b", parent="a")
-    static abstract class T extends Mission {
+    static abstract class Y extends Mission {
         public void method() {
             @DefineScope(name="a", parent="b")
             @Scope("a")
