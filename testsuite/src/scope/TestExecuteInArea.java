c2c3a02d8c8bedfbe632b3151671f65f97efef41
aplsek <aplsek@gmail.com>
2011-03-03 14:08:04 -0500

    -n executeInArea - test-case scj.jar updated

--- testsuite/src/scope/TestExecuteInArea.java
+++ testsuite/src/scope/TestExecuteInArea.java
@@ -1,8 +1,3 @@
-//scope/TestExecuteInArea.java:17: Runnable and PrivateMemory scopes disagree.
-//        a.executeInArea(r);
-//                       ^
-//1 error
-
 package scope;
 
 import javax.safetycritical.ManagedMemory;
@@ -13,33 +8,42 @@ import javax.safetycritical.annotate.RunsIn;
 import javax.safetycritical.annotate.Scope;
 import javax.safetycritical.annotate.DefineScope;
 
+import static checkers.scope.ScopeChecker.ERR_BAD_EXECUTE_IN_AREA_OR_ENTER;
+import static checkers.scope.ScopeChecker.ERR_BAD_EXECUTE_IN_AREA_TARGET;
 import static javax.safetycritical.annotate.Scope.IMMORTAL;
 
 @Scope("a")
 @DefineScope(name = "a", parent = IMMORTAL)
 public abstract class TestExecuteInArea extends Mission {
 
-    //PrivateMemory b;
-    //PrivateMemory c;
-
-   // GoodR r2 = new GoodR();
-
-    //@RunsIn("c")
-    //public void bar() {
-    //    a.executeInArea(r2);
-   // }
+    @Scope("c")
+    @DefineScope(name = "c", parent = "b")
+    static abstract class X extends Mission {}
 
     @Scope("b")
     @DefineScope(name = "b", parent = "a")
     abstract static class x extends Mission {
 
-        @DefineScope(name = "b", parent = "a")
-        @Scope("a")
+        @DefineScope(name = "a", parent = IMMORTAL)
+        @Scope(IMMORTAL)
         ManagedMemory a;
+
+        @DefineScope(name = "c", parent = "b")
+        @Scope("b")
+        ManagedMemory c;
 
         public void m() {
             Run r = new Run();
             a.executeInArea(r);
+
+            Run2 r2 = new Run2();
+            a.executeInArea(r2);
+
+            Run3 r3 = new Run3();
+            a.executeInArea(r3);
+
+            Run4 r4 = new Run4();
+            c.executeInArea(r4);
         }
     }
 
@@ -50,13 +54,27 @@ public abstract class TestExecuteInArea extends Mission {
         }
     }
 
-    /*
-    @Scope(IMMORTAL)
-    static class GoodR implements Runnable {
-        @RunsIn(IMMORTAL)
+    @Scope("b")
+    static class Run2 implements SCJRunnable {
+        //## checkers.scope.ScopeChecker.ERR_BAD_EXECUTE_IN_AREA_OR_ENTER
+        @RunsIn("b")
         public void run() {
         }
-    }*/
+    }
 
+    @Scope("b")
+    static class Run3 implements SCJRunnable {
+        //## checkers.scope.ScopeChecker.ERR_RUNNABLE_WITHOUT_RUNS_IN
+        public void run() {
+        }
+    }
+
+    @Scope("b")
+    static class Run4 implements SCJRunnable {
+        //## checkers.scope.ScopeChecker.ERR_BAD_EXECUTE_IN_AREA_TARGET
+        @RunsIn("c")
+        public void run() {
+        }
+    }
 }
 
