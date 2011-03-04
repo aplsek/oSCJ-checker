cb5869b22a50b2f8a2ac03a90748c79a8bfd3cdb
aplsek <aplsek@gmail.com>
2011-03-04 15:43:45 -0500

    -n TestBadMethodInvocation - empty test-case, will write it today.

---
+++ testsuite/src/scope/scope/simple/TestBadMethodInvocation.java
@@ -0,0 +1,12 @@
+package scope.scope.simple;
+
+public class TestBadMethodInvocation {
+
+
+    //ERR_BAD_METHOD_INVOKE
+
+    // ERR_BAD_METHOD_INVOKE
+
+    // check parameters!!
+    //ERR_BAD_ASSIGNMENT_SCOPE
+}
