5bd587c6541d632646406893eb3a7d9baf472ff4
aplsek <aplsek@gmail.com>
2011-03-04 19:45:20 -0500

    -n TestBadMethodInvocation

--- testsuite/src/scope/scope/simple/TestBadMethodInvocation.java
+++ testsuite/src/scope/scope/simple/TestBadMethodInvocation.java
@@ -1,12 +1,64 @@
 package scope.scope.simple;
 
-public class TestBadMethodInvocation {
+import javax.safetycritical.Mission;
+import javax.safetycritical.annotate.DefineScope;
+import javax.safetycritical.annotate.RunsIn;
+import javax.safetycritical.annotate.Scope;
+
+@DefineScope(name="a", parent=Scope.IMMORTAL)
+@Scope("a")
+public abstract class TestBadMethodInvocation extends Mission {
+
+    @Scope("a")
+    @DefineScope(name="b", parent="a")
+    static abstract class X extends Mission {
+
+        Y y;
+
+        @RunsIn("b")
+        public void method() {
+            method();
+            y.methodUNK();
+            y.methodRunsInB();
+
+            //## checkers.scope.ScopeChecker.ERR_BAD_METHOD_INVOKE
+            y.method();
+            //## checkers.scope.ScopeChecker.ERR_BAD_METHOD_INVOKE
+            y.methodRunsInC();
+        }
+
+        void method2() {
+            y.method();
+        }
+
+    }
+
+    @Scope("a")
+    static class Y {
+        void method() {
+        }
+
+        @RunsIn(Scope.UNKNOWN)
+        void methodUNK() {
+        }
+
+        @RunsIn("b")
+        void methodRunsInB() {
+
+        }
+
+        @RunsIn("c")
+        void methodRunsInC() {
+
+        }
+    }
+
+    @Scope("c")
+    @DefineScope(name="c", parent="b")
+    static abstract class W extends Mission {}
+
+    // TODO: check parameters!!
+    // ERR_BAD_ASSIGNMENT_SCOPE
 
 
-    //ERR_BAD_METHOD_INVOKE
-
-    // ERR_BAD_METHOD_INVOKE
-
-    // check parameters!!
-    //ERR_BAD_ASSIGNMENT_SCOPE
 }
