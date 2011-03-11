35aafc7117973a9e8ff434de24a352904d5ae664
aplsek <aplsek@gmail.com>
2011-03-11 11:47:40 -0500

    -n adding a testCase for UnusedDefineScope.

---
+++ testsuite/src/scope/defineScope/simple/TestUnusedDefineScope2.java
@@ -0,0 +1,9 @@
+package scope.defineScope.simple;
+
+import static javax.safetycritical.annotate.Scope.IMMORTAL;
+
+import javax.safetycritical.annotate.DefineScope;
+
+@DefineScope(name="a", parent=IMMORTAL)
+//## warning: checkers.scope.DefineScopeChecker.ERR_UNUSED_DEFINE_SCOPE
+public class TestUnusedDefineScope2 { }
