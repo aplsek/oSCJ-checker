b059c3f6f9a568c6a94a7881583a0e54b3f974df
aplsek <aplsek@gmail.com>
2011-03-21 16:04:17 -0400

    -n s

---
+++ testsuite/src/scope/scope/sanity/TestNHRTParameters.java
@@ -0,0 +1,36 @@
+package scope.scope.sanity;
+
+import javax.realtime.PriorityParameters;
+
+import javax.safetycritical.NoHeapRealtimeThread;
+import javax.safetycritical.StorageParameters;
+
+import javax.safetycritical.annotate.DefineScope;
+import javax.safetycritical.annotate.RunsIn;
+import javax.safetycritical.annotate.Scope;
+import javax.safetycritical.annotate.SCJAllowed;
+import static javax.safetycritical.annotate.Level.LEVEL_2;
+import static javax.safetycritical.annotate.Scope.IMMORTAL;
+
+@Scope("D")
+@DefineScope(name="D", parent=IMMORTAL)
+@SCJAllowed(value= LEVEL_2, members=true)
+public class TestNHRTParameters extends NoHeapRealtimeThread
+{
+  private static final long BackingStoreRequirements = 500;
+  private static final long NativeStackRequirements = 2000;
+  private static final long JavaStackRequirements = 300;
+
+  public TestNHRTParameters(int priority) {
+    super(new PriorityParameters(priority),
+          new StorageParameters(BackingStoreRequirements,
+                                NativeStackRequirements,
+                                JavaStackRequirements));
+  }
+
+  @Override
+  @RunsIn("D")
+  public void run() {
+  }
+}
+
