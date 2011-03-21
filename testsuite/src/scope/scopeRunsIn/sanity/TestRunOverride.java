8fb50385e36f53e64a40e081da244df7e20e5347
aplsek <aplsek@gmail.com>
2011-03-21 16:03:13 -0400

    -n sanity check of overriding of @RunsIn on run method.

---
+++ testsuite/src/scope/scopeRunsIn/sanity/TestRunOverride.java
@@ -0,0 +1,32 @@
+package scope.scopeRunsIn.sanity;
+
+import javax.realtime.PriorityParameters;
+
+import javax.safetycritical.NoHeapRealtimeThread;
+import javax.safetycritical.StorageParameters;
+
+import javax.safetycritical.annotate.DefineScope;
+import javax.safetycritical.annotate.RunsIn;
+import javax.safetycritical.annotate.Scope;
+import javax.safetycritical.annotate.SCJAllowed;
+import static javax.safetycritical.annotate.Level.LEVEL_2;
+import static javax.safetycritical.annotate.Scope.IMMORTAL;
+
+@Scope("D")
+@DefineScope(name="D", parent=IMMORTAL)
+@SCJAllowed(value= LEVEL_2, members=true)
+public class TestRunOverride extends NoHeapRealtimeThread
+{
+  public TestRunOverride(int priority) {
+    super(new PriorityParameters(priority),
+          new StorageParameters(0,
+                                0,
+                                0));
+  }
+
+  @Override
+  @RunsIn("D")
+  public void run() {
+  }
+}
+
