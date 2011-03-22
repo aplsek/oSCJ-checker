7c401a88c54d73e0bd059ec0fd2855f9eb30af7b
aplsek <aplsek@gmail.com>
2011-03-22 16:02:28 -0400

    -n Test framework cleanup.
    Sanity and Simple tests now run all the tests from these two packages.
    use the 'ant simple-tests' to run these test-cases.

---
+++ testsuite/src/tests/SanityTest.java
@@ -0,0 +1,24 @@
+package tests;
+
+import java.io.File;
+import java.util.Collection;
+
+import org.junit.runners.Parameterized.Parameters;
+
+public class SanityTest extends ParameterizedCheckerTest {
+
+    public SanityTest(File testFile) {
+        super(testFile, "checkers.SCJChecker", "framework", "-Anomsgtext");
+    }
+
+    @Parameters
+    public static Collection<Object[]> data() {
+        Collection<Object[]> tests = testFiles("scope/defineScope/sanity");
+
+        tests.addAll(testFiles("scope/scopeRunsIn/sanity"));
+        tests.addAll(testFiles("scope/scope/sanity"));
+        tests.addAll(testFiles( "scjAllowed/sanity"));
+
+        return tests;
+    }
+}
