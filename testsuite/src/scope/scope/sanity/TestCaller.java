6df34fe5a87a5a2cc578ceffb6e30759366af79e
aplsek <aplsek@gmail.com>
2011-03-22 15:35:51 -0400

    -n Two test cases added into scope/scope/sanity
    TestPrimitiveAssignment - reveals a bug when manipulating primitive values.
    TestCAller - what should be the annotation for the AbosoluteTime.set method?

---
+++ testsuite/src/scope/scope/sanity/TestCaller.java
@@ -0,0 +1,36 @@
+package scope.scope.sanity;
+
+import static javax.safetycritical.annotate.Scope.CALLER;
+import static javax.safetycritical.annotate.Scope.IMMORTAL;
+import static javax.safetycritical.annotate.Scope.UNKNOWN;
+
+import javax.realtime.AbsoluteTime;
+import javax.safetycritical.Mission;
+import javax.safetycritical.annotate.DefineScope;
+import javax.safetycritical.annotate.RunsIn;
+import javax.safetycritical.annotate.SCJAllowed;
+import javax.safetycritical.annotate.Scope;
+
+@Scope("D")
+@DefineScope(name="D", parent=IMMORTAL)
+@SCJAllowed(members=true)
+public class TestCaller extends Mission {
+
+    AbsoluteTime update_time;
+    AbsoluteTime prior_update_time;
+
+    // called periodically by the GPS Driver
+    @RunsIn(CALLER)
+    synchronized void updatePosition(@Scope(UNKNOWN) AbsoluteTime time_stamp) {
+      update_time.set(time_stamp);          // ERROR ???
+    }
+
+    @Override
+    public long missionMemorySize() {
+        return 0;
+    }
+
+    @Override
+    protected void initialize() {
+    }
+}
---
+++ testsuite/src/scope/scope/sanity/TestPrimitiveAssignment.java
@@ -0,0 +1,39 @@
+package scope.scope.sanity;
+
+import static javax.safetycritical.annotate.Scope.CALLER;
+import static javax.safetycritical.annotate.Scope.IMMORTAL;
+
+import javax.safetycritical.Mission;
+import javax.safetycritical.annotate.DefineScope;
+import javax.safetycritical.annotate.RunsIn;
+import javax.safetycritical.annotate.SCJAllowed;
+import javax.safetycritical.annotate.Scope;
+
+@Scope("D")
+@DefineScope(name="D", parent=IMMORTAL)
+@SCJAllowed(members=true)
+public class TestPrimitiveAssignment extends Mission {
+
+    private int prior_longitude, prior_lattitude;
+    private int longitude, lattitude;
+
+    // called periodically by the GPS Driver
+    @RunsIn(CALLER)
+    synchronized void updatePosition(int longitude, int lattitude) {
+      prior_longitude = this.longitude;
+      prior_lattitude = this.lattitude;
+
+      this.longitude = longitude;
+      this.lattitude = lattitude;
+
+    }
+
+    @Override
+    public long missionMemorySize() {
+        return 0;
+    }
+
+    @Override
+    protected void initialize() {
+    }
+}
