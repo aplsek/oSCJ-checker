76e007d292569da2f9f30d090faeaf2c4edebe76
Daniel Tang <dytang@cs.purdue.edu>
2011-03-29 10:40:59 -0400

    -n undoing classpath modification in check.sh
    This is already handled by localbin/javac

--- examples/check.sh
+++ examples/check.sh
@@ -4,6 +4,6 @@ set -e
 set -x
 cd .. && ./build.sh && cd examples/
 
-../localbin/javac -proc:only -cp ../localbin/jdk.jar:../lib/scj.jar:../lib/scj-checker.jar:../lib/scj-jdk-spec.jar  -processor checkers.SCJChecker `find ./railsegment -name "*.java"`
+../localbin/javac -proc:only -cp ../lib/scj.jar:../lib/scj-checker.jar  -processor checkers.SCJChecker `find ./railsegment -name "*.java"`
 
 
