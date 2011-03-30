4d41ac6febc44a5b8164ce2e0a7e2e39d9997ae0
aplsek <aplsek@gmail.com>
2011-03-30 17:59:10 -0400

    -n thruster build script minor update

--- examples/thruster.sh
+++ examples/thruster.sh
@@ -4,6 +4,6 @@ set -e
 set -x
 cd .. && ./build.sh && cd examples/
 
-../localbin/javac -proc:only -cp ../lib/scj.jar:../lib/scj-checker.jar  -processor checkers.SCJChecker `find ./thruster/src -name "*.java" -not -path "*tmp*" -not -path "*button*"`
+../localbin/javac -proc:only -cp ../lib/scj.jar:../lib/scj-checker.jar  -processor checkers.SCJChecker `find ./thruster/src -name "*.java"`
 
 
