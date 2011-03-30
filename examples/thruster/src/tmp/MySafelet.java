c5fe579fca0663c79aaca226a2a6696909e9b130
aplsek <aplsek@gmail.com>
2011-03-30 16:24:47 -0400

    -n thruster - minor

--- examples/thruster/src/tmp/MySafelet.java
+++ examples/thruster/src/tmp/MySafelet.java
@@ -1,12 +1,16 @@
 package tmp;
 
 import static javax.safetycritical.annotate.Level.LEVEL_1;
+import static javax.safetycritical.annotate.Level.SUPPORT;
 import static javax.safetycritical.annotate.Phase.INITIALIZATION;
+import static javax.safetycritical.annotate.Scope.IMMORTAL;
 
 import javax.safetycritical.MissionSequencer;
 import javax.safetycritical.Safelet;
+import javax.safetycritical.annotate.RunsIn;
 import javax.safetycritical.annotate.SCJAllowed;
 import javax.safetycritical.annotate.SCJRestricted;
+import javax.safetycritical.annotate.Scope;
 
 
 /**
@@ -17,6 +21,7 @@ import javax.safetycritical.annotate.SCJRestricted;
  *
  */
 @SCJAllowed(value = LEVEL_1, members=true)
+@Scope(IMMORTAL)
 public class MySafelet implements Safelet {
 
     @SCJRestricted(INITIALIZATION)
@@ -30,17 +35,23 @@ public class MySafelet implements Safelet {
 	 * "myMissionSequencer" is null, create a new MissionSequencer, otherwise
 	 * return the "myMissionSequencer" directly.
 	 */
+    @Override
+    @SCJAllowed(SUPPORT)
+    @SCJRestricted(INITIALIZATION)
+    @Scope(IMMORTAL) @RunsIn(IMMORTAL)
 	public MissionSequencer getSequencer() {
-		//System.out.println("TestCase 02: PASS. MissionSequencer.getSequencer() is executed.");
-		return MyMissionSequencer.getInstance();
-		//return null;
-	}
+      //System.out.println("TestCase 02: PASS. MissionSequencer.getSequencer() is executed.");
+        return MyMissionSequencer.getInstance();
+        //return null;
+    }
 
 	/**
 	 * This method will be called before "getSequencer()". This method may
 	 * create objects in ImmortalMemoryArea, set up hardware interrupt handlers,
 	 * or initializing hardware devices.
 	 */
+    @SCJAllowed(SUPPORT)
+    @SCJRestricted(INITIALIZATION)
 	public void setUp() {
 		/*
 		 * This testing method doesn't contain any operation, just print
@@ -53,6 +64,8 @@ public class MySafelet implements Safelet {
 	 * This method will be called after the termination of the MissionSequencer.
 	 * It frees all resources used by the application.
 	 */
+    @SCJAllowed(SUPPORT)
+    @SCJRestricted(INITIALIZATION)
 	public void tearDown() {
 		/*
 		 * This testing method doesn't contain any operation, just print
