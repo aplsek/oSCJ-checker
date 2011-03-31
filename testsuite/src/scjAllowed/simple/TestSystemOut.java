d894b53bb44fac5285d5b079da6d17c5a85381f7
aplsek <aplsek@gmail.com>
2011-03-31 18:14:53 -0400

    -n test-case for System.out - not no errors, which is Ok.

---
+++ testsuite/src/scjAllowed/simple/TestSystemOut.java
@@ -0,0 +1,13 @@
+package scjAllowed.simple;
+
+import static javax.safetycritical.annotate.Level.LEVEL_0;
+
+import javax.safetycritical.annotate.SCJAllowed;
+
+@SCJAllowed(members=true, value=LEVEL_0)
+public class TestSystemOut {
+
+    public void run() {
+        System.out.println(new String(""));
+    }
+}
