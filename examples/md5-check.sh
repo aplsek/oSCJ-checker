06dc88f38cbfd7373e07381d2470b078119167d2
aplsek <aplsek@gmail.com>
2011-03-31 12:49:41 -0400

    -n build files using ant jar

--- examples/md5-check.sh
+++ examples/md5-check.sh
@@ -2,7 +2,7 @@
 
 set -e
 set -x
-cd .. && ./build.sh && cd examples/
+cd .. && ant jar && cd examples/
 
 ../localbin/javac -proc:only -cp ../lib/scj.jar:../lib/scj-checker.jar  -processor checkers.SCJChecker `find ./fast-md5/src -name "*.java"`
 
