0b0776fce73b3950cf22fed382731ec5f875e4be
aplsek <aplsek@gmail.com>
2011-03-31 13:05:26 -0400

    -n checker - adding classes into spec

---
+++ spec/java/io/PrintStream.java
@@ -0,0 +1,15 @@
+package java.io;
+
+import javax.safetycritical.annotate.SCJAllowed;
+import javax.safetycritical.annotate.RunsIn;
+import javax.safetycritical.annotate.Scope;
+import static javax.safetycritical.annotate.Scope.CALLER;
+import static javax.safetycritical.annotate.Scope.UNKNOWN;
+
+
+@SCJAllowed
+public class PrintStream {
+
+    @RunsIn(CALLER) 
+    public void print(@Scope(UNKNOWN) String s) {}
+}
---
+++ spec/java/util/Random.java
@@ -0,0 +1,25 @@
+/*
+ * @(#)Random.java  1.47 06/02/07
+ *
+ * Copyright 2006 Sun Microsystems, Inc. All rights reserved.
+ * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package java.util;
+import java.io.*;
+import java.util.concurrent.atomic.AtomicLong;
+
+import javax.safetycritical.annotate.SCJAllowed;
+
+import sun.misc.Unsafe;
+
+
+@SCJAllowed
+public
+class Random implements java.io.Serializable {
+   
+    @SCJAllowed
+    public float nextFloat() {
+        return 0;
+    }
+}
