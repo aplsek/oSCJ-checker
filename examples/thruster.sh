53d0791f4ed12a35873bd1d8b2580300f4c8e1af
Daniel Tang <dytang@cs.purdue.edu>
2011-04-01 11:07:57 -0400

    -n allow extra parameters on checking scripts

--- examples/thruster.sh
+++ examples/thruster.sh
@@ -4,6 +4,6 @@ set -e
 set -x
 cd .. && ant jar && cd examples/
 
-../localbin/javac -proc:only -cp ../lib/scj.jar:../lib/scj-checker.jar  -processor checkers.SCJChecker `find ./thruster/src -name "*.java"`
+../localbin/javac -proc:only -cp ../lib/scj.jar:../lib/scj-checker.jar  -processor checkers.SCJChecker "$@" `find ./thruster/src -name "*.java"`
 
 
