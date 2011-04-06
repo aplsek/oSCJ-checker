15eddecda45425ab9f566bff078c1d281c40cb2a
aplsek <aplsek@gmail.com>
2011-04-06 16:03:25 -0400

    -n jpapabench - build parameters updated

--- examples/jpapabench/build.sh
+++ examples/jpapabench/build.sh
@@ -5,7 +5,7 @@ set -x
 
 
 FIJI_HOME="/home/plsek/fiji/fivm/"
-SCJFLAGS="--scj --scj-scope-backing 1500k --g-def-immortal-mem 2330k --g-scope-checks no --pollcheck-mode none"   #700 scope, 500 imm
+SCJFLAGS="--scj --scj-scope-backing 13500k --g-def-immortal-mem 12330k --g-scope-checks no --pollcheck-mode none"   #700 scope, 500 imm
 FIJIFLAGS="--max-threads 3 --more-opt"  # -v 1
 
 # rebuild SCJ.jar                 
