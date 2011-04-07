0c4f410ca9657f32f2cd9eb768eebdc24e882155
aplsek <aplsek@gmail.com>
2011-04-07 17:08:41 -0400

    -n adding SCJSchedulable class - was missing from the previous commit
    railsegment script for checking the railsegment added.

---
+++ src/checkers/SCJSchedulable.java
@@ -0,0 +1,41 @@
+package checkers;
+
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.TypeMirror;
+import javax.lang.model.util.Elements;
+import javax.lang.model.util.Types;
+
+public enum SCJSchedulable {
+    PEH("javax.safetycritical.PeriodicEventHandler", "handleAsyncEvent"),
+    APEH("javax.safetycritical.APeriodicEventHandler", "handleAsyncEvent"),
+    MANAGED_THREAD("javax.safetyCritical.ManagedThread", "run"),
+    DEFAULT(null, null);
+
+    public final String clazz;
+    public final String signature;
+
+    SCJSchedulable(String clazz, String signature) {
+        this.clazz = clazz;
+        this.signature = signature;
+    }
+
+    @Override
+    public String toString() {
+        return clazz + "." + signature;
+    }
+
+    public static SCJSchedulable fromMethod(TypeElement t, Elements elements,
+            Types types) {
+        TypeMirror m = t.asType();
+
+        for (SCJSchedulable sm : SCJSchedulable.values()) {
+            if (sm.equals(DEFAULT) || sm == null)
+                continue;
+            TypeMirror schedulable = Utils.getTypeMirror(elements, sm.clazz);
+            if (types.isSubtype(m, schedulable))
+                return sm;
+        }
+
+        return DEFAULT;
+    }
+}
