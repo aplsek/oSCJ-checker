1bf2796f5c09cf0185316054c1ef887e9a263b4b
aplsek <aplsek@gmail.com>
2011-04-14 19:25:36 -0400

    -n test-cases updated.

---
+++ testsuite/src/scjAllowed/sanity/TestMissionOverride.java
@@ -0,0 +1,69 @@
+package scjAllowed.sanity;
+
+/**
+ *  Name: Railsegment
+ *  Author : Kelvin Nilsen, <kelvin.nilsen@atego.com>
+ *
+ *  Copyright (C) 2011  Kelvin Nilsen
+ *
+ *  Railsegment is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  Railsegment is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with Railsegment; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+import static javax.safetycritical.annotate.Level.LEVEL_2;
+import static javax.safetycritical.annotate.Level.SUPPORT;
+import static javax.safetycritical.annotate.Phase.INITIALIZATION;
+import static javax.safetycritical.annotate.Scope.CALLER;
+
+import javax.safetycritical.Mission;
+import javax.safetycritical.annotate.RunsIn;
+import javax.safetycritical.annotate.SCJAllowed;
+import javax.safetycritical.annotate.SCJRestricted;
+import javax.safetycritical.annotate.Scope;
+
+
+
+@SCJAllowed(value=LEVEL_2, members=true)
+public class TestMissionOverride extends Mission
+{
+  @SCJRestricted(INITIALIZATION)
+  public TestMissionOverride() {
+    // nothing much happens here
+  }
+
+  @Override
+  @SCJAllowed
+  public final long missionMemorySize()
+  {
+    // must be large enough to represent the three Schedulables
+    // instantiated by the initialize() method
+    return 0;
+  }
+
+  @Override
+  @SCJRestricted(INITIALIZATION)
+  @SCJAllowed(SUPPORT)
+  public void initialize() {
+  }
+
+  @Override
+  @RunsIn(CALLER)
+  //// @SCJAllowed
+  @SCJAllowed(value=LEVEL_2)
+  public void requestTermination()
+  {
+    notifyAll();
+    super.requestTermination();
+  }
+}
---
+++ testsuite/src/scope/schedulable/simple/TestMissionInit2.java
@@ -0,0 +1,86 @@
+package scope.schedulable.simple;
+
+import static javax.safetycritical.annotate.Phase.INITIALIZATION;
+import static javax.safetycritical.annotate.Scope.IMMORTAL;
+
+import javax.realtime.PeriodicParameters;
+import javax.realtime.PriorityParameters;
+import javax.safetycritical.CyclicExecutive;
+import javax.safetycritical.CyclicSchedule;
+import javax.safetycritical.PeriodicEventHandler;
+import javax.safetycritical.StorageParameters;
+import javax.safetycritical.annotate.DefineScope;
+import javax.safetycritical.annotate.RunsIn;
+import javax.safetycritical.annotate.SCJAllowed;
+import javax.safetycritical.annotate.SCJRestricted;
+import javax.safetycritical.annotate.Scope;
+import static javax.safetycritical.annotate.Level.SUPPORT;
+
+@SCJAllowed(members=true)
+@Scope("a")
+@DefineScope(name="a", parent=IMMORTAL)
+public class TestMissionInit2 extends CyclicExecutive {
+
+    @SCJRestricted(INITIALIZATION)
+    public TestMissionInit2() {
+        super(null);
+    }
+
+    @Override
+    @SCJAllowed(SUPPORT)
+    public CyclicSchedule getSchedule(PeriodicEventHandler[] handlers) {
+        return null;
+    }
+
+    @Override
+    @SCJRestricted(INITIALIZATION)
+    @SCJAllowed(SUPPORT)
+    public void initialize() {
+        new PEH(null,null,null);
+        new PEH(null,null,null);
+
+        method();
+    }
+
+    @SCJRestricted(INITIALIZATION)
+    private void method() {
+        /// ERROR
+        new PEH(null,null,null);
+    }
+
+    @Override
+    @SCJAllowed(SUPPORT)
+    public void setUp() {
+    }
+
+    @Override
+    @SCJAllowed(SUPPORT)
+    public void tearDown() {
+    }
+
+
+    @Scope("a")
+    @SCJAllowed(members=true)
+    @DefineScope(name = "b", parent = "a")
+    public class PEH extends PeriodicEventHandler {
+
+        @SCJRestricted(INITIALIZATION)
+        public PEH(PriorityParameters priority,
+                PeriodicParameters period, StorageParameters storage) {
+            super(priority, period, storage);
+        }
+
+        @Override
+        @RunsIn("b")
+        @SCJAllowed(SUPPORT)
+        public void handleAsyncEvent() {
+        }
+    }
+
+
+    @Override
+    public long missionMemorySize() {
+        return 0;
+    }
+
+}
