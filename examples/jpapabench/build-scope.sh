73f72a3604578fef4ef2d49516fe9117d71ec393
aplsek <aplsek@gmail.com>
2011-04-25 09:43:45 -0400

    -n build scripts for benchmarks.

---
+++ examples/jpapabench/build-scope.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+set -x
+
+
+FIJI_HOME="/home/plsek/fiji/fivm/"
+SCJFLAGS="--scj --scj-scope-backing 13500k --g-def-immortal-mem 12330k --pollcheck-mode none"   #700 scope, 500 imm
+FIJIFLAGS="--max-threads 3 --more-opt"  # -v 1
+
+# rebuild SCJ.jar                 
+SCJ="../../../../scj/ri"
+CWD=`pwd`
+cd $SCJ && make scj.jar && cd $CWD
+
+# CLEAN-UP
+rm -rf build
+mkdir build
+
+# COMPILE & JAR
+find ./jpapabench-jpaparazzi/jpapabench-core/src -name "*.java" > list
+find ./jpapabench-jpaparazzi/jpapabench-core-flightplans/src -name "*.java" >> list
+find ./jpapabench-jpaparazzi/jpapabench-scj/src -name "*.java" -not -name "*Level1*" >> list
+javac -cp $FIJI_HOME/lib/scj.jar -d build/ @list	
+cd build/ && find . -name "*.class" | xargs jar cf ../papabench.jar && cd ..
+rm -rf list
+
+# 
+SAFELET=papabench.scj.PapaBenchSCJLevel0Application
+#SAFELET=papabench.scj.HelloWorld
+
+# COMPILE FIJI
+$FIJI_HOME/bin/fivmc -o papabench-scope --scj-safelet $SAFELET $FIJIFLAGS $SCJFLAGS papabench.jar
+	
+	
+# RUN:	
+./papabench-scope | tee papabench-scope.cap
+
+
+
