bf0393f3e52c5231a7936e7cacd762dcb3cf5011
aplsek <aplsek@gmail.com>
2011-06-06 16:36:01 -0400

    -n minor update.

--- testsuite/src/scope/scopeRunsIn/sanity/TestRunnable.java
+++ testsuite/src/scope/scopeRunsIn/sanity/TestRunnable.java
@@ -5,18 +5,13 @@ import static javax.safetycritical.annotate.Scope.IMMORTAL;
 
 import javax.realtime.PriorityParameters;
 import javax.safetycritical.annotate.Scope;
-import javax.safetycritical.ManagedThread;
 import javax.safetycritical.MissionSequencer;
 import javax.safetycritical.StorageParameters;
 
 import javax.safetycritical.annotate.DefineScope;
 import javax.safetycritical.annotate.RunsIn;
 import javax.safetycritical.annotate.SCJRestricted;
-import javax.safetycritical.annotate.Scope;
 import javax.safetycritical.annotate.SCJAllowed;
-import static javax.safetycritical.annotate.Level.LEVEL_2;
-import static javax.safetycritical.annotate.Level.SUPPORT;
-import static javax.safetycritical.annotate.Scope.IMMORTAL;
 
 
 @SCJAllowed(members=true)
