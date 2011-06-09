a05cc398cf4a787da8c13a7e2681c49ae021e9ff
aplsek <aplsek@gmail.com>
2011-06-09 10:07:02 -0400

    -n test-cases for default level parameter.

---
+++ examples/level1-app-noSCJAllowed.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+set -x
+cd .. && ant jar && cd examples/
+
+../localbin/javac -proc:only -cp ../lib/scj.jar:../lib/scj-checker.jar  -processor checkers.SCJChecker "$@" `find ./level1-app-noSCJAllowed -name "*.java"`
