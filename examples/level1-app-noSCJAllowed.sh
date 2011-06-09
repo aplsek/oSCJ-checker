22d9183ad7c88903b0db810af3414645789700a1
aplsek <aplsek@gmail.com>
2011-06-09 10:07:46 -0400

    -n minor changes.

--- examples/level1-app-noSCJAllowed.sh
+++ examples/level1-app-noSCJAllowed.sh
@@ -4,4 +4,4 @@ set -e
 set -x
 cd .. && ant jar && cd examples/
 
-../localbin/javac -proc:only -cp ../lib/scj.jar:../lib/scj-checker.jar  -processor checkers.SCJChecker "$@" `find ./level1-app-noSCJAllowed -name "*.java"`
+../localbin/javac -proc:only -cp ../lib/scj.jar:../lib/scj-checker.jar  -processor checkers.SCJChecker "$@" `find ./level1-app-noSCJAllowed -name "*.java"` -Alevel=1
