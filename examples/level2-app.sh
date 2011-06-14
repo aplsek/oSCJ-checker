414524f67c4b3e26068c9161cb29fad24bc1ba39
aplsek <aplsek@gmail.com>
2011-06-14 14:45:38 -0400

    -n cleanup, all HelloWorld examples moved to test-cases.

--- examples/level2-app.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-set -x
-cd .. && ant jar && cd examples/
-
-../localbin/javac -proc:only -cp ../lib/scj.jar:../lib/scj-checker.jar  -processor checkers.SCJChecker "$@" `find ./level2-app -name "*.java"`
